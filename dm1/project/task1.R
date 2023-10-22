77a84d88c00493906769f816b45366113020df12
hugens <hugens@localhost.localdomain>
2023-10-22 19:05:49 +0100

    started working on Data Mining I Project

---
+++ dm1/project/task1.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+
+setwd("~/cc/dm1/project")
+
+accounts <- read_csv2("bank/account.csv")
+card_dev <- read_csv2("bank/card_dev.csv")
+client <- read_csv2("bank/client.csv")
+disp <- read_csv2("bank/disp.csv")
+district <- read_csv2("bank/district.csv")
+loan_dev <- read_csv2("bank/loan_dev.csv")
+trans_dev <- read_csv2("bank/trans_dev.csv")
+
+
