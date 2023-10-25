dd3fcaf404a2dda8808f348bee7aa8fabcbcc90a
hugens <hugens@localhost.localdomain>
2023-10-25 20:41:26 +0100

    distribution of submatrices is working, at least for N=6

--- cp/ass1/mpi_distribute.c
+++ cp/ass1/mpi_distribute.c
@@ -3,12 +3,17 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define ROOT 0
+#define TAG 0
+
 int main(int argc, char *argv[]){
     int P, my_rank;
     int N, Q, S;
     double Q_candidate;
     int* submat;
     int* mat;
+
+    MPI_Status status;
 
     // MPI Inits
     MPI_Init(&argc, &argv);
@@ -18,9 +23,7 @@ int main(int argc, char *argv[]){
     Q_candidate = sqrt(P);
     Q = (int)Q_candidate;
 
-    //int *submat = (int*) malloc(S*Sdd)
-    
-    if(my_rank == 0){
+    if(my_rank == ROOT){
         scanf("%d", &N);
 
         if(Q_candidate != Q || N % Q != 0){
@@ -33,50 +36,48 @@ int main(int argc, char *argv[]){
 
     S = N / Q;
 
-    submat = (int*) malloc(S*S*sizeof(int));
+    submat = (int*) malloc(S * S * sizeof(int));
     mat = (int *) malloc(N * N * sizeof(int));
 
-    if(my_rank==0){
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++)
+    //printf("Process %d, N=%d, Q=%d, S=%d\n", my_rank, N, Q, S);
+
+    if(my_rank == ROOT){
+        printf("Process %d\n", 0);
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++){
                 scanf("%d", &mat[i * N + j]);
+                printf("%d ", mat[i * N + j]);
+            }
+            printf("\n");
+        }
+        printf("\n");
 
         int proc = 0;
         for(int i_init=0; i_init<=S; i_init+=S){
             for(int j_init=0; j_init<=S; j_init+=S){
-                proc++;
+                //printf("proc=%d\n", proc);
                 for(int i=0; i<S; i++){
                     for(int j=0; j<S; j++){
                         submat[i * S + j] = mat[(i_init + i) * N + (j_init + j)];
+                        //printf("%d ", submat[i * S + j]);
                     }
+                    //printf("\n");
                 }
-                MPI_Send(submat, S*S, MPI_INT, proc, proc, MPI_COMM_WORLD);
+                //MPI_Send(submat, S*S, MPI_INT, proc, proc, MPI_COMM_WORLD);
+                MPI_Send(submat, S*S, MPI_INT, proc, TAG, MPI_COMM_WORLD);
+                //MPI_Send(data, 2, MPI_INT, proc, TAG, MPI_COMM_WORLD);
+                proc++;
             }
         }
     }
-    //int MPI_Send(const void *buf, int count, MPI_Datatype datatype, int dest, int tag, MPI_Comm comm)
-    //int MPI_Recv(void *buf, int count, MPI_Datatype datatype, int source, int tag, MPI_Comm comm, MPI_Status *status)
-
-    for(int proc=0; i<P; proc++){
-        MPI_Recv(submat, S*S, MPI_INT, 0, tag, MPI_COMM_WORLD, status);
-    }
-
     //MPI_Bcast(mat, N*N, MPI_INT, 0, MPI_COMM_WORLD);
 
-    printf("Process %d, N=%d, Q=%d, S=%d\n", my_rank, N, Q, S);
+    MPI_Recv(submat, S*S, MPI_INT, ROOT, TAG, MPI_COMM_WORLD, &status); // works
 
-    //printf("submat:\n");
-    //for(int i=0; i<S; i++){
-    //    for(int j=0; j<S; j++){
-    //        printf("%d ", submat[i*S+j]);
-    //    }
-    //    printf("\n");
-    //}
-
-    printf("mat:\n");
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            printf("%d ", mat[i*N+j]);
+    printf("Process %d, submat:\n", my_rank);
+    for(int i=0; i<S; i++){
+        for(int j=0; j<S; j++){
+            printf("%d ", submat[i*S+j]);
         }
         printf("\n");
     }
