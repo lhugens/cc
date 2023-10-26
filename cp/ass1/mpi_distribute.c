dea1f922c0902652dd29eb7d508deee48efa7bd5
hugens <hugens@localhost.localdomain>
2023-10-26 12:46:58 +0100

    started actual fox algorithm

--- cp/ass1/mpi_distribute.c
+++ cp/ass1/mpi_distribute.c
@@ -5,12 +5,62 @@
 
 #define ROOT 0
 #define TAG 0
+#define min(a, b) a < b ? a : b
+
+void print(int n, int *D){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            if(D[i * n + j]==-1){
+                printf("%d ", 0);
+            } else {
+                printf("%d ", D[i * n + j]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+int special_vector_mult(int n, int x[], int y[]){
+    int c = -1;
+    for(int k=0; k<n; k++){
+        if(x[k]!=-1 && y[k]!=-1){
+            if(c != -1){
+                c = min(c, x[k]+y[k]);
+            } else {
+                c = x[k]+y[k];
+            }
+       }
+    }
+    return c;
+}
+
+int* special_matrix_mult(int n, int *A, int *B){
+    int *C, *row, *col;
+    C = (int *) malloc(n * n * sizeof(int));
+    row = (int *) malloc(n * sizeof(int));
+    col = (int *) malloc(n * sizeof(int));
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            row[j] = A[i * n + j];
+        }
+        for(int jp=0; jp<n; jp++){
+            for(int k=0; k<n; k++){
+                col[k] = B[k * n + jp];
+            }
+            C[i * n + jp] = special_vector_mult(n, row, col);
+        }
+    }
+    free(row);
+    free(col);
+    return C;
+} 
 
 int main(int argc, char *argv[]){
     int P, my_rank;
     int N, Q, S;
     double Q_candidate;
-    int* submat;
+    int* submatB;
+    int* submatA;
     int* mat;
 
     MPI_Status status;
@@ -36,7 +86,7 @@ int main(int argc, char *argv[]){
 
     S = N / Q;
 
-    submat = (int*) malloc(S * S * sizeof(int));
+    submatB = (int*) malloc(S * S * sizeof(int));
     mat = (int *) malloc(N * N * sizeof(int));
 
     if(my_rank == ROOT){
@@ -66,27 +116,77 @@ int main(int argc, char *argv[]){
     int i_init = coordinates[0]*S;
     int j_init = coordinates[1]*S;
 
+    printf("Process %d\n", my_rank);
     printf("my_rank=%d, my_grid_rank=%d, coordinates=[%d,%d]\n", my_rank, my_grid_rank, coordinates[0], coordinates[1]);
 
     //////////////////////////////////////////////
 
     for(int i=0; i<S; i++){
         for(int j=0; j<S; j++){
-            submat[i * S + j] = mat[(i_init + i) * N + (j_init + j)];
+            submatB[i * S + j] = mat[(i_init + i) * N + (j_init + j)];
         }
     }
 
-    printf("Process %d, submat:\n", my_rank);
-    for(int i=0; i<S; i++){
-        for(int j=0; j<S; j++){
-            printf("%d ", submat[i*S+j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
+    //////////////////////////////////////////////
+    
+    // ROW COL COMMS
+    int varying_coords[2];
+    // create row communicators
+    MPI_Comm row_comm;
+    varying_coords[0] = 0; varying_coords[1] = 1;
+    MPI_Cart_sub(grid_comm, varying_coords, &row_comm);
+    int row_rank;
+    MPI_Cart_rank(row_comm, coordinates, &row_rank);
+    // create column communicators
+    MPI_Comm col_comm;
+    varying_coords[0] = 1; varying_coords[1] = 0;
+    MPI_Cart_sub(grid_comm, varying_coords, &col_comm);
+    int col_rank;
+    MPI_Cart_rank(col_comm, coordinates, &col_rank);
 
-    free(submat);
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    printf("Global Rank: %d, Row Rank: %d\n", my_rank, row_rank);
+
+    int step=0;
+
+
+    int r = row_rank;
+    int u = (r + step) % Q;
+    int* chosen_coords = (int*) malloc(2*sizeof(int));
+    chosen_coords[0] = r;
+    chosen_coords[1] = u;
+
+    int step2_root;
+    MPI_Cart_rank(grid_comm, chosen_coords, &step2_root);
+
+    //int* submatC = (int*) malloc(S * S * sizeof(int));
+    
+    printf("r=%d, u=%d -> Chose submatrix in proc: step2_root=%d\n", r, u, step2_root);
+
+    printf("Process before%d, submatB:\n", my_rank);
+    print(S, submatB);
+
+    MPI_Bcast(submatB, S*S, MPI_INT, step2_root, grid_comm);
+    
+    MPI_Barrier(MPI_COMM_WORLD);
+
+
+    //////////////////////////////////////////////
+
+    printf("Process after%d, submatB:\n", my_rank);
+    print(S, submatB);
+    //for(int i=0; i<S; i++){
+    //    for(int j=0; j<S; j++){
+    //        printf("%d ", submatB[i*S+j]);
+    //    }
+    //    printf("\n");
+    //}
+    //printf("\n");
+
     free(mat);
+    free(submatB);
+    free(submatA);
 
     MPI_Finalize();
 
