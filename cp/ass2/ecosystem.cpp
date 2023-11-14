3b074d1bb185e2c92f48d947351c3dd763a5c761
Leonardo Hügens <leonardohugens@protonmail.com>
2023-11-14 09:16:06 +0000

    ecosystem.cpp - fixed pointer problems

--- cp/ass2/ecosystem.cpp
+++ cp/ass2/ecosystem.cpp
@@ -21,19 +21,11 @@ struct Ecosystem {
         scanf("%d", &C);
         scanf("%d", &N);
 
-        printf("%d ", GEN_PROC_RABBITS);
-        printf("%d ", GEN_PROC_FOXES);
-        printf("%d ", GEN_FOOD_FOXES);
-        printf("%d ", N_GEN);
-        printf("%d ", R);
-        printf("%d ", C);
-        printf("%d\n", N);
-
         WORLD = (char*) malloc(R*C*sizeof(char));
 
         for(int i=0; i<R; i++)
             for(int j=0; j<C; j++)
-                WORLD[i*R+j] = ' ';
+                WORLD[i*R+j] = '_';
 
         char name[6];
         int x;
@@ -49,18 +41,80 @@ struct Ecosystem {
                 WORLD[x*R+y] = 'F';
             }
         }
+    }
 
+    void print_world(){
         for(int i=0; i<R; i++){
             for(int j=0; j<C; j++){
-                printf("%c", WORLD[i*R+j]);
+                printf("%c ", WORLD[i*C+j]);
             }
             printf("\n");
         }
+    }
 
+    void move_rabbits(){
+        int *coords;
+        coords = (int*) malloc(2*sizeof(int));
+        for(int i=0; i<R; i++){
+            for(int j=0; j<C; j++){
+                if(WORLD[i*C+j] == 'R'){
+                    if (select_adjacent_cell(i, j, '_', coords)){
+                        printf("chosen (%d, %d)\n", coords[0], coords[1]); 
+                    }
+                }
+            }
+        }
+    }
+
+    bool select_adjacent_cell(int x, int y, char object, int *chosen_coords){
+        int P = 0;
+        int x_p;
+        int y_p;
+        int possible_x[4];
+        int possible_y[4];
+
+        // north
+        x_p = x-1; y_p = y;
+        if(is_inside(x_p, y_p) && WORLD[x_p*C+y_p] == object){
+            possible_x[P] = x_p; possible_y[P] = y_p; P++;
+        }
+        // east
+        x_p = x; y_p = y+1;
+        if(is_inside(x_p, y_p) && WORLD[x_p*C+y_p] == object){
+            possible_x[P] = x_p; possible_y[P] = y_p; P++;
+        }
+        // south
+        x_p = x+1; y_p = y;
+        if(is_inside(x_p, y_p) && WORLD[x_p*C+y_p] == object){
+            possible_x[P] = x_p; possible_y[P] = y_p; P++;
+        }
+        // west
+        x_p = x; y_p = y-1;
+        if(is_inside(x_p, y_p) && WORLD[x_p*C+y_p] == object){
+            possible_x[P] = x_p; possible_y[P] = y_p; P++;
+        }
+
+        int chosen = (0 + x + y) % P;
+        printf("chosen %d, move to (%d, %d)\n", chosen, possible_x[chosen], possible_y[chosen]);
+        if(P>0){
+            chosen_coords[0] = possible_x[chosen];
+            chosen_coords[1] = possible_y[chosen];
+            return true;
+        }
+        return false;
+    }
+
+    bool is_inside(int i, int j){
+        if(i!=-1 && i!=R+1 && j!=-1 && j!=C+1){
+            return true;
+        }
+        return false;
     }
 };
 
 int main(){
     Ecosystem E;
+    E.print_world();
+    E.move_rabbits();
     return 0;
 }
